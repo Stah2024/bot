5a40f1cd65b40247357302eb66306d04acfb4e90
Stah2024 <stahcoin@gmail.com>
2025-11-10 23:20:46 +0300

    Update models.py

--- db/models.py
+++ db/models.py
@@ -11,6 +11,6 @@ class UserConnection(Base):
     id = Column(Integer, primary_key=True)
     telegram_id = Column(Integer, nullable=False, unique=True)
     tg_token = Column(String, nullable=False)
-    vk_token = Column(String, nullable=False)
+    vkken = Column(String, nullable=False)
     vk_group_id = Column(Integer, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
