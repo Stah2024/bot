7ceaa30a1cb066bc1a9de6b447702793fbb1eea2
Stah2024 <stahcoin@gmail.com>
2025-11-10 17:56:09 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,10 +1,9 @@
 from aiogram import types
 
 def get_main_keyboard():
-    kb = types.InlineKeyboardMarkup(row_width=1)
-    kb.add(
-        types.InlineKeyboardButton("Подключить", callback_data="connect"),
-        types.InlineKeyboardButton("Оплатить", callback_data="pay"),
-        types.InlineKeyboardButton("Инструкция", callback_data="help")
-    )
-    return kb
+    buttons = [
+        [types.InlineKeyboardButton("Подключить", callback_data="connect")],
+        [types.InlineKeyboardButton("Оплатить", callback_data="pay")],
+        [types.InlineKeyboardButton("Инструкция", callback_data="help")]
+    ]
+    return types.InlineKeyboardMarkup(inline_keyboard=buttons)
