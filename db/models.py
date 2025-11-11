8b0786d0073c86db6c33200f4971851ed6de67c1
Stah2024 <stahcoin@gmail.com>
2025-11-11 11:41:07 +0300

    Update models.py

--- db/models.py
+++ db/models.py
@@ -1,6 +1,5 @@
-# db/models.py
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Column, Integer, String, LargeBinary, DateTime
+from sqlalchemy import Column, Integer, String, LargeBinary, DateTime, BigInteger
 from datetime import datetime
 
 class Base(DeclarativeBase):
@@ -11,12 +10,15 @@ class UserConnection(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     telegram_id = Column(Integer, nullable=False, unique=True, index=True)
-    
+
     # ТОКЕНЫ — ЗАШИФРОВАННЫЕ (bytes)
     tg_token = Column(LargeBinary, nullable=False)    # ← зашифровано
     vk_token = Column(LargeBinary, nullable=False)    # ← зашифровано
-    
+
     # ID группы ВК — СТРОКА с минусом
     vk_group_id = Column(String, nullable=False)      # ← "-123456789"
-    
+
+    # ID Telegram-канала, откуда идёт репост
+    channel_id = Column(BigInteger, nullable=True, unique=True)
+
     created_at = Column(DateTime, default=datetime.utcnow)
