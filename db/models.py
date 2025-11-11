41ec2b0862512ea22b91a43b33202a5ec4644f78
Stah2024 <stahcoin@gmail.com>
2025-11-11 17:32:31 +0300

    Update models.py

--- db/models.py
+++ db/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Column, Integer, String, LargeBinary, DateTime, BigInteger
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class Base(DeclarativeBase):
     pass
@@ -9,15 +9,24 @@ class UserConnection(Base):
     __tablename__ = "user_connections"
 
     id = Column(Integer, primary_key=True, index=True)
-    telegram_id = Column(Integer, nullable=False, unique=True, index=True)
 
-    # ТОКЕН ВК — ЗАШИФРОВАННЫЙ (bytes)
+    # Telegram user ID
+    telegram_id = Column(BigInteger, nullable=False, unique=True, index=True)
+
+    # VK токен (зашифрованный)
     vk_token = Column(LargeBinary, nullable=False)
 
-    # ID группы ВК — СТРОКА с минусом
+    # VK group ID (строка с минусом)
     vk_group_id = Column(String, nullable=False)
 
-    # ID Telegram-канала, откуда идёт репост
-    channel_id = Column(BigInteger, nullable=True, unique=True)
+    # Telegram channel ID (откуда идёт репост)
+    channel_id = Column(BigInteger, nullable=True, unique=True, index=True)
 
+    # Дата создания записи
     created_at = Column(DateTime, default=datetime.utcnow)
+
+    # Дата оплаты
+    paid_at = Column(DateTime, default=datetime.utcnow)
+
+    # Срок действия подписки
+    expires_at = Column(DateTime, default=lambda: datetime.utcnow() + timedelta(days=7))
