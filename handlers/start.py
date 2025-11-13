0e2c26801cd4c8242675ad9be748c64f2f64e01a
Stah2024 <stahcoin@gmail.com>
2025-11-13 14:37:00 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -5,6 +5,7 @@ def get_main_keyboard():
         inline_keyboard=[
             [InlineKeyboardButton(text="Подключить", callback_data="connect")],
             [InlineKeyboardButton(text="Оплатить", callback_data="pay")],
-            [InlineKeyboardButton(text="Инструкция", callback_data="help")]
+            [InlineKeyboardButton(text="Инструкция", callback_data="help")],
+            [InlineKeyboardButton(text="Поддержка", callback_data="support")]
         ]
     )
