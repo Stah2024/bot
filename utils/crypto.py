c548b631b9b593e5d9d84144efbc3a31798c0ec4
Stah2024 <stahcoin@gmail.com>
2025-11-13 18:28:42 +0300

    Update crypto.py

--- utils/crypto.py
+++ utils/crypto.py
@@ -1,7 +1,7 @@
 from cryptography.fernet import Fernet
 from config import CRYPTO_KEY
 
-fernet = Fernet(CRYPTO_KEY.encode())
+fernet = Fernet(CRYPTO_KEY)  # ✅ ключ уже bytes, encode не нужен
 
 def encrypt(text: str) -> bytes:
     return fernet.encrypt(text.encode())
